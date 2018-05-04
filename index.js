00ddd1cbfbd7fe1e2053c7f4dc207bc3b31ac105
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 19:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 class BoardMember {
   constructor(name, homeState, training) {
-    this.name = name 
-    this.homeState = homeState 
-    this.training = training 
+    this.name = name
+    this.homeState = homeState
+    this.training = training
   }
-  
+
   veto(){
     return 'No, I must disagree'
   }
@@ -21,5 +21,11 @@ class BoardMember {
 
   sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
+
+class Ceo extends BoardMember {
+  hireEmployee() {
+    return "Welcome aboard!"
   }
 }
